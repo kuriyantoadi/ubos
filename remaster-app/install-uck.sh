7ed723985997b70a6c4685ef304fcfbfa95e5bad
kuriyantoadi <melatikuning212@gmail.com>
2018-08-02 08:56:07 +0700

    update 2 agustus

--- remaster-app/install-uck.sh
+++ remaster-app/install-uck.sh
@@ -1,4 +1,8 @@
 #sudo apt update -y
 #sudo apt install dialog zenity kdebase-bin genisoimage dpkg-dev build-essential fakeroot gfxboot squashfs-tools bzr mktemp uck -y
-cat perintah-uck.md >> ~/.bashrc
+#cat perintah-uck.md >> ~/.bashrc
 
+sudo apt-add-repository ppa:cubic-wizard/release
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6494C6D6997C215E
+sudo apt update
+sudo apt install cubic
